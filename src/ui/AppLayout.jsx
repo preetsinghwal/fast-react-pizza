7a49ced098ca518fdbe17550abf22db91c7e3b7c
preet singhwal <preet.singhwal@tothenew.com>
2024-11-03 23:40:17 +0530

    created project architecture, util, pages componenets, etc

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverView from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
+
+export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <CartOverView />
+    </div>
+  );
+}
