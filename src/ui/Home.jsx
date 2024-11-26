fdb2c2be2344cb9db1890bcdc4d247e007256607
preet <preet.singhwal@tothenew.com>
2024-11-26 16:37:05 +0530

    dummy

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
       <h1 className="text-stone-700 text-center text-xl font-semibold">
         The best pizza.
         <br />
-        <span className="text-yellow-500">Straight out of the oven, straight to you.</span>
+        <span className="text-yellow-500">Straight out of the oven, straight to you data. </span>
       </h1>
     </div>
   );
