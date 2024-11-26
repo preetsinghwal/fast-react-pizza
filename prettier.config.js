e67a490bcb62eb91999c86c0e634dd5b6e388c0f
preet singhwal <preet.singhwal@tothenew.com>
2024-11-26 13:19:56 +0530

    tailwind css

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+// prettier.config.js
+const config = {
+    plugins: ['prettier-plugin-tailwindcss'],
+    singleQuote: true,
+    jsxSingleQuote: true
+  };
+   
+export default config;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,9 +3,10 @@ import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
     return (
-        <header>
+        <header className="bg-yellow-500">
             <Link to="/">Fast React Pizza Co.</Link>
             <SearchOrder />
+            <p>Preet</p>
         </header>
     )
 }
